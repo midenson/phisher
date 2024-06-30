1390d27b547dbd959d0fa480c80a11246b12a6a3
midenson <olatunjinaheem012@gmail.com>
2024-06-30 21:37:54 +0000

    made  changes

--- phisher/components/Signingoogle.jsx
+++ phisher/components/Signingoogle.jsx
@@ -5,23 +5,39 @@ import React from "react";
 
 
 
-const signupwithoauth = async () => {
-  const supabase_url = 'https://dkabkqkvvoiykaouhrvc.supabase.co';
-  const supabase_anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrYWJrcWt2dm9peWthb3VocnZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg4OTg4MzMsImV4cCI6MjAzNDQ3NDgzM30.Zm5SUO2H4NYHbwi4XksMwWtJq7KO8YpiXFHaHj-89Mg';
-  const supabase = createClient(
-    supabase_url,
-    supabase_anon
-  );
+// const signupwithoauth = async () => {
+//   const supabase_url = 'https://dkabkqkvvoiykaouhrvc.supabase.co';
+//   const supabase_anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrYWJrcWt2dm9peWthb3VocnZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg4OTg4MzMsImV4cCI6MjAzNDQ3NDgzM30.Zm5SUO2H4NYHbwi4XksMwWtJq7KO8YpiXFHaHj-89Mg';
+//   const supabase = createClient(
+//     supabase_url,
+//     supabase_anon
+//   );
 
-  const { error } = await supabase.auth.signInWithOAuth({
-    provider: 'apple'
-  });
-  if (!error) {
-    redirect('./')
-  }
-}
+//   const { error } = await supabase.auth.signInWithOAuth({
+//     provider: 'apple'
+//   });
+//   if (!error) {
+//     redirect('./')
+//   }
+// }
 
 const Signingoogle = ({ btnText, icon }) => {
+  const signupwithoauth = async () => {
+    const supabase_url = 'https://dkabkqkvvoiykaouhrvc.supabase.co';
+    const supabase_anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrYWJrcWt2dm9peWthb3VocnZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg4OTg4MzMsImV4cCI6MjAzNDQ3NDgzM30.Zm5SUO2H4NYHbwi4XksMwWtJq7KO8YpiXFHaHj-89Mg';
+    const supabase = createClient(
+      supabase_url,
+      supabase_anon
+    );
+  
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'apple'
+    });
+    if (!error) {
+      redirect('./')
+    }
+  }
+  
   return (
     <button
     onClick={signupwithoauth}
